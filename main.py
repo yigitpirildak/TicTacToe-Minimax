16f518f1386778b54cae0499f5c0c26ddd19d88c
Eclipse <yigitpirildak@gmail.com>
2020-05-03 23:27:06 +0300

    Add board evaluation function. Highlight winning marks on the board. Remove commented-out code from main

--- main.py
+++ main.py
@@ -18,35 +18,8 @@ def draw():
     size_per_square = SIZE/3
     ticTacBoard.draw()
 
-
     if mouse_is_pressed:
         ticTacBoard.make_move(mouse_x, mouse_y)
 
 if __name__ == "__main__":
     run()
-    """memory = Memory()
-    model = TicTacModel()
-    for i in range(0, 20):
-        ticTacGame = TicTacGame()
-        terminal = False
-        turnToPlay = Mark.X
-        x_train = np.array([])
-        y_train = np.array([])
-        while not terminal:
-            print("Turn To Play = " + turnToPlay.name)
-            rewards = np.ndarray(9)
-            current_state = ticTacGame.get_normalized_board_with_player(turnToPlay)
-            prediction = int(np.argmax(model.predict(np.array([ticTacGame.get_normalized_board_with_player(turnToPlay)]))))
-            actual_reward, terminal = ticTacGame.make_move((int(prediction / 3), int(prediction % 3)), turnToPlay)
-            while actual_reward == -100:
-                rewards[prediction] = actual_reward
-                prediction = random.randrange(0, 9)
-                actual_reward, terminal = ticTacGame.make_move((int(prediction / 3), int(prediction % 3)), turnToPlay)
-            rewards[prediction] = actual_reward
-            x_train = np.insert(x_train, x_train.size, current_state)
-            y_train = np.insert(y_train, y_train.size, rewards / max(abs(np.max(rewards)), abs(np.min(rewards))))
-
-            turnToPlay = Mark.X if turnToPlay == Mark.O else Mark.O
-        ticTacGame.print_board()
-        model.train(x_train.reshape(int(x_train.size / 10), 10), y_train.reshape(int(y_train.size / 9), 9))
-"""
--- ticTacBoard.py
+++ ticTacBoard.py
@@ -4,9 +4,14 @@ from p5 import *
 FONT = create_font("fonts/SigmarOne-Regular.ttf", 0)
 
 class Mark(Enum):
-    X = 1
-    O = 2
-    EMPTY = 3
+    X = 2
+    O = 4
+    EMPTY = 8
+
+class State(Enum):
+    DRAW = 1
+    ONGOING = 2
+    OVER = 3
 
 class TicTacBoard:
     def __init__(self, size):
@@ -15,6 +20,7 @@ class TicTacBoard:
         self.boardMatrix = [[Mark.EMPTY.value for x in range(3)] for y in range(3)]
         self.turnToPlay = Mark.X
         self.mark_size = int(self.size_per_square / 2)
+        self.winningMarks = []
 
     def draw(self):
         self.__draw_background()
@@ -22,6 +28,7 @@ class TicTacBoard:
 
     def __draw_background(self):
         stroke_weight(5)
+        stroke(0)
         line((self.size_per_square, 0), (self.size_per_square, self.size))
         line((self.size_per_square * 2, 0), (self.size_per_square * 2, self.size))
         line((0, self.size_per_square), (self.size, self.size_per_square))
@@ -34,6 +41,10 @@ class TicTacBoard:
                 if state is not Mark.EMPTY:
                     text_font(FONT)
                     text_size(self.mark_size)
+                    if (x, y) in self.winningMarks:
+                        fill(127, 0, 0)
+                    else :
+                        fill(255)
                     text(state.name, (self.size_per_square * x + self.size_per_square / 2 - self.mark_size / 2, self.size_per_square * y + self.size_per_square / 2 - self.mark_size))
 
     def make_move(self, mouse_x, mouse_y):
@@ -43,5 +54,41 @@ class TicTacBoard:
             self.boardMatrix[x][y] = self.turnToPlay.value
             self.__switch_players()
 
+            board_eval = self.evaluate_board_state()
+            print(board_eval)
+            if (board_eval[0] is State.OVER):
+                self.winningMarks = board_eval[2:]
+
     def __switch_players(self):
         self.turnToPlay = Mark.X if self.turnToPlay is Mark.O else Mark.O
+
+    def evaluate_board_state(self):
+        draw = True
+        for x in range(3):
+            for y in range(3):
+                mark = Mark(self.boardMatrix[x][y])
+                if mark is Mark.EMPTY:
+                    draw = False
+                    continue
+                else:
+                    # Horizontal
+                    try:
+                        if (mark.value | self.boardMatrix[x+1][y] | self.boardMatrix[x+2][y]) == mark.value:
+                            return (State.OVER, mark, (x, y), (x+1, y), (x+2, y))
+                    except:
+                        pass
+
+                    # Vertical
+                    try:
+                        if (mark.value | self.boardMatrix[x][y+1] | self.boardMatrix[x][y+2]) == mark.value:
+                            return (State.OVER, mark, (x, y), (x, y+1), (x, y+2))
+                    except:
+                        pass
+
+                    # Diagonal
+                    if x == 0 and y == 0 and (mark.value | self.boardMatrix[x+1][y+1] | self.boardMatrix[x+2][y+2] == mark.value):
+                        return (State.OVER, mark, (x, y), (x+1, y+1), (x+2, y+2))
+                    elif x == 0 and y == 2 and (mark.value | self.boardMatrix[x+1][y-1] | self.boardMatrix[x+2][y-2] == mark.value):
+                        return (State.OVER, mark, (x, y), (x+1, y-1), (x+2, y-2))
+
+        return [State.DRAW if draw else State.ONGOING]
